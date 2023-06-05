d433046315a670636e96ce632587d01b2e05f422
28263-pm <127601914+28263-pm@users.noreply.github.com>
2023-06-05 11:07:20 +0200

    Update Weze.sql

--- Weze.sql
+++ Weze.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Czas generowania: 04 Kwi 2023, 17:27
--- Wersja serwera: 10.4.21-MariaDB
--- Wersja PHP: 7.4.26
+-- Host: 127.0.0.1
+-- Generation Time: Cze 05, 2023 at 11:00 AM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,40 +18,63 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `Weze`
+-- Database: `weze`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `Games`
+-- Struktura tabeli dla tabeli `games`
 --
 
-CREATE TABLE `Games` (
+CREATE TABLE `games` (
   `id_games` int(11) NOT NULL,
-  `game_name` varchar(200) COLLATE utf8_bin NOT NULL,
-  `game_link` varchar(200) COLLATE utf8_bin NOT NULL,
-  `png_link` varchar(200) COLLATE utf8_bin NOT NULL
+  `game_name` varchar(200) NOT NULL,
+  `game_link` varchar(999) NOT NULL,
+  `png_link` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Zrzut danych tabeli `Games`
+-- Dumping data for table `games`
 --
 
-INSERT INTO `Games` (`id_games`, `game_name`, `game_link`, `png_link`) VALUES
-(1, 'Roll a ball', 'build/index.html', 'Img/Roll.png'),
-(2, 'Angry Birds', 'AngryBirds/AngryBirds/index.html', 'Img/Angry.png'),
-(6, '2D Platformer', '2DPlatformer/index.html', 'Img/2D.png');
+INSERT INTO `games` (`id_games`, `game_name`, `game_link`, `png_link`) VALUES
+(1, 'RollABall', 'Gry/RollABall/index.html', 'Gry/Img/Roll.png'),
+(2, 'Angry Birds', 'Gry/AngryBirds/index.html', 'Gry/Img/Angry.png'),
+(6, '2D Platformer', 'Gry/Platformer/index.html', 'Gry/Img/2D.png');
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `Leaderboard`
+-- Struktura tabeli dla tabeli `games2`
 --
 
-CREATE TABLE `Leaderboard` (
+CREATE TABLE `games2` (
+  `game_id` int(11) NOT NULL,
+  `game_name` varchar(50) NOT NULL,
+  `png_link` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `games2`
+--
+
+INSERT INTO `games2` (`game_id`, `game_name`, `png_link`) VALUES
+(2, 'RollABall', ''),
+(3, 'AngryBirds', ''),
+(4, '2DPlatformer', ''),
+(7, '', ''),
+(8, '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `leaderboard`
+--
+
+CREATE TABLE `leaderboard` (
   `id_leaderboard` int(200) NOT NULL,
-  `user_name` varchar(20) COLLATE utf8_bin NOT NULL,
+  `user_name` varchar(20) NOT NULL,
   `highscore` int(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -63,33 +86,41 @@ CREATE TABLE `Leaderboard` (
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `name` varchar(20) COLLATE utf8_bin NOT NULL,
-  `pass` varchar(200) COLLATE utf8_bin NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `pass` varchar(200) NOT NULL,
   `admin` int(2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Zrzut danych tabeli `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `pass`, `admin`) VALUES
 (10, 'Admin', '81dc9bdb52d04dc20036dbd8313ed055', 1),
-(11, 'Szymon', '81dc9bdb52d04dc20036dbd8313ed055', NULL);
+(11, 'Szymon', '81dc9bdb52d04dc20036dbd8313ed055', NULL),
+(12, 'Michal', '202cb962ac59075b964b07152d234b70', 1),
+(13, 'test', '098f6bcd4621d373cade4e832627b4f6', NULL);
 
 --
 -- Indeksy dla zrzutów tabel
 --
 
 --
--- Indeksy dla tabeli `Games`
+-- Indeksy dla tabeli `games`
 --
-ALTER TABLE `Games`
+ALTER TABLE `games`
   ADD PRIMARY KEY (`id_games`);
 
 --
--- Indeksy dla tabeli `Leaderboard`
+-- Indeksy dla tabeli `games2`
 --
-ALTER TABLE `Leaderboard`
+ALTER TABLE `games2`
+  ADD PRIMARY KEY (`game_id`);
+
+--
+-- Indeksy dla tabeli `leaderboard`
+--
+ALTER TABLE `leaderboard`
   ADD PRIMARY KEY (`id_leaderboard`),
   ADD UNIQUE KEY `user_name` (`user_name`);
 
@@ -101,26 +132,32 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `name` (`name`);
 
 --
--- AUTO_INCREMENT dla zrzuconych tabel
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT dla tabeli `Games`
+-- AUTO_INCREMENT for table `games`
 --
-ALTER TABLE `Games`
-  MODIFY `id_games` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+ALTER TABLE `games`
+  MODIFY `id_games` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT dla tabeli `Leaderboard`
+-- AUTO_INCREMENT for table `games2`
 --
-ALTER TABLE `Leaderboard`
+ALTER TABLE `games2`
+  MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `leaderboard`
+--
+ALTER TABLE `leaderboard`
   MODIFY `id_leaderboard` int(200) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT dla tabeli `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
